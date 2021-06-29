c98ea96ce65a078554e3f785d88aa5d0f7da0013
Megumin25 <44318059+Megumin25@users.noreply.github.com>
2021-06-29 23:52:09 +0200

    Update Discord Injections.js

--- Discord Injections.js
+++ Discord Injections.js
@@ -1,30 +1,5 @@
-document.write(`
-    <button id="inject">Inject</button>
-    <button id="noVerify">noVerify</button>
-    <button id="fetchToken">fetch token</button>
-    <button id="extractToken">extract token</button>
-`);
-
-document.querySelector("#inject").onclick = () => login(prompt("Token:"))
-document.querySelector('#noVerify').onclick = () => login(retrieve())
-document.querySelector("#fetchToken").onclick = () => display.innerText = retrieve()
-document.querySelector("#extractToken").onclick = () => alert(document.body.appendChild(document.createElement('iframe')).contentWindow.localStorage.token)
-
-
-function login(token) {
-    setInterval(_=> document.body.appendChild(document.createElement('iframe')).contentWindow.localStorage.token = `"${token}"`, 50); 
-    setTimeout(_=> location.reload(), 2500)
-}
-
-async function retrieve() {
-    return (await (await fetch("https://discord.com/api/v8/auth/login", {
-        method:'POST',
-        body: JSON.stringify({
-            'email': prompt('Email:'),
-            'password': prompt('Password:')
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })).json()).token
-}
+document.write('<button id="sT">Set Token</button><button id="gT">Get Token</button>')
+let Discord = Object.values(webpackJsonp.push([[], { '_': (a, _, c) => a.exports = c}, [['_']]]).c)
+.find(x => x?.exports?.default?.setToken).exports.default
+document.querySelector("#sT").onclick = () => {Discord.setToken(prompt('Token:')); location.reload()}
+document.querySelector("#gT").onclick = () => alert(Discord.getToken())
